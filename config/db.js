6fae28b665fa15403276de8541144a3f9a8e10f9
Nisna Tanna <nisna3@gmail.com>
2026-04-14 11:21:56 -0400

    config file changes

--- config/db.js
+++ config/db.js
@@ -1,11 +1,23 @@
 const mysql = require("mysql2");
-// Connect to MySQL
-const db = mysql.createPool({
+const fs = require("fs");
+const path = require("path");
+
+const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+    database: process.env.DB_NAME,
+};
 
-module.exports = db.promise()
+// ONLY add SSL if we are NOT on our local machine
+if (process.env.NODE_ENV === "production") {
+    dbConfig.ssl = {
+        rejectUnauthorized: false,
+        ca: fs.readFileSync(path.join(__dirname, "../global-bundle.pem"))
+    };
+}
+
+const db = mysql.createPool(dbConfig);
+
+module.exports = db.promise();
 
